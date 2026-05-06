6ab1316291cbf029f76a2035cad338153395b0a0
ChristianMcBride <chmcbride@ctemc.org>
2026-05-06 13:18:33 -0400

    Vex project

---
+++ McBride-vex/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Owner                                                        #
+# 	Created:      5/4/2026, 2:15:01 PM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
